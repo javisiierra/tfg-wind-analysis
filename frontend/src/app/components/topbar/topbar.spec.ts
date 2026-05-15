04e5c7d17486017477b9a21a24f946f41909598e
javisiierra <jsm422@alumnos.unican.es>
2026-05-15 14:09:44 +0200

    Estado del caso automatico

--- frontend/src/app/components/topbar/topbar.spec.ts
+++ frontend/src/app/components/topbar/topbar.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { Topbar } from './topbar';
 
@@ -9,6 +11,7 @@ describe('Topbar', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Topbar],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Topbar);
